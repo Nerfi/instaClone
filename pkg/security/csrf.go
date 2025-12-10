0bd0463558bfb6ba3fe3b86c96b3da0d9abce522
Juan Paredes <jfparede@aicrumit.com>
2025-12-10 13:57:47 +0100

    CSRF file implementation

--- pkg/security/csrf.go
+++ pkg/security/csrf.go
@@ -1,1 +1,11 @@
 package security
+
+import (
+	"net/http"
+
+	"github.com/gorilla/csrf"
+)
+
+func NewCSRF(secret []byte, secure bool) func(http.Handler) http.Handler {
+	return csrf.Protect(secret, csrf.Secure(secure))
+}
